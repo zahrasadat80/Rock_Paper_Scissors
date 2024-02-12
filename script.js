69d96ba4ea18c7a0563f1f3d16995fccfb094e69
zahra <99148531+zahrasadat80@users.noreply.github.com>
2024-02-12 15:51:35 +0330

    add all functions and they work correctly.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 //variables
-let botScores = 0;
-let playerScores = 0;
+let scores = [0,0];
+
 let message;
 let playerChoice = '';
 let botChoice = '';
+let botScore=0;
+let playerScore=0;
 
 //query selectors
 let buttons = document.querySelectorAll('.btn');
 let playerChoiceText = document.querySelector('#your-selection');
 let botChoiceText = document.querySelector('#bot-selection');
-let scores = document.querySelector('#scores');
+let scoresText = document.querySelector('#scores');
+let container = document.querySelector('.container');
 
 
 //add event listener
@@ -17,10 +20,13 @@ let scores = document.querySelector('#scores');
 buttons.forEach(function(btn){
     btn.addEventListener('click', function(){ 
         playerChoice = btn.value;
-        console.log(playerChoice);
+        console.log(`player:${playerChoice}`); // test of player choice
+        countPoints();
+        determineWinner();
+        
      } )  
 })
-
+//its ok
 
 
 
@@ -40,27 +46,77 @@ function gettingBotChoice(){
     return botChoice;
 }
 //test bot choice
-console.log(gettingBotChoice());
-function compareChoices(botChoice,playerChoice){
-    //logic for who wins in each round
+// console.log(gettingBotChoice()); // its OK
 
+
+function compareChoices(getBotChoice,PlayerChoice){
+    //logic for who wins in each round
+    botChoice = getBotChoice();
+    console.log(`bot:${botChoice}`);
+    if (playerChoice==='rock' && botChoice==='paper'){
+        return 0;
+    }
+    else if (playerChoice==='paper' && botChoice === 'rock'){
+        return 1;
+    }
+    else if (playerChoice ==='paper'&& botChoice === 'scissors'){
+        return 0;
+    }
+    else if (playerChoice === 'scissors' && botChoice === 'paper'){
+        return 1;
+    }
+    else if (playerChoice ==='scissors' && botChoice === 'rock'){
+         return 0;
+    }
+    else if (playerChoice ==='rock' && botChoice === 'scissors'){
+        return 1;
+    }
+    return -1;
     //return 0 for bot point return 1 for player point 
 }
-function countPoints(compareChoices){
+function countPoints(){
     //if compare choice is 0 => add to point of bot 
+    let result = compareChoices(gettingBotChoice,playerChoice);
+    if (result===0){
+        scores[0]++;
+    }
+    else if(result===1){
+        scores[1]++;
+    }
+    return scores;
     //if compare choice is 1 => add to points of player
     //return an array from points [bot,player]
 }
-function determineWinner(countPoints){
+function determineWinner(){
     //if array[0] and array[1] was lower than 5 console => bot = array[0] , player = array[1]  ;
+    if (scores[0]<5 && scores[1]<5){
+        console.log (`your scores = ${scores[1]} and bot scores = ${scores[0]}`);
+    }
     //if array[0] >= 5 console => bot = array[0] , player = array[1] and message = 'you lose , try one more time
+    else if (scores[0]>=5){
+        console.log('you lose, Try again');
+        restartGame();
+    }
     //if array[1] >= 5 consol =>  bot = array[0] , player = array[1] and message = you win , congratulation
+    else if (scores[1]>=5){
+        console.log(`you win , congratulation`);
+        restartGame();
+    }
 }
 
 function restartGame (){
     let restartBtn = document.createElement('button');
     restartBtn.innerText = 'restart';
+    restartBtn.classList.add('restartBtn');
     //append it to message
+    container.appendChild(restartBtn);
+    restartBtn.addEventListener('click',function(){
+        scores[0]=0;
+        scores[1]=0;
+        restartBtn.remove();
+    })
+    
+
 }
 
 
